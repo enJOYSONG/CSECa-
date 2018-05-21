820b4cc5e8748dbadc5a24f7504f0d09517b1d67
enJOYSONG <tmdgml5156@nafe>
2018-05-21 15:20:30 +0900

    join, login form 추가

--- SKHUeClass/account/views.py
+++ SKHUeClass/account/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render, redirect
 from rest_framework.views import APIView
+from django.views.decorators.csrf import csrf_protect
 
 def main(request):
     if request.method == "GET":
         return render(request, 'login.html')
 
-
+@csrf_protect
 def login(request):
     if request.method == "GET":
         return render(request, 'login.html')
     if request.method == "POST":
-        return redirect('/lecture')
+        return redirect('/lecture/lecture_list')
 
-
+@csrf_protect
 def join(request):
     if request.method == "GET":
         return render(request, 'join.html')
