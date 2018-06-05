0ca86277a017a56955fd1bfee6a343b97ca49733
KJH\KilJH <roadjh0201@gmail.com>
2018-06-05 14:32:10 +0900

    user student 모델 수정* isTA

--- SKHUeClass/account/models.py
+++ SKHUeClass/account/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 
+
+
 # Create your models here.
 class BaseUser(models.Model):
     id = models.CharField(max_length=20, primary_key=True)
     email = models.EmailField(max_length=100)
     password = models.CharField(max_length=100)
 
+
 class Professor(models.Model):
     base_user = models.OneToOneField(BaseUser, on_delete=models.CASCADE)
 
+
 class Student(models.Model):
     base_user = models.OneToOneField(BaseUser, on_delete=models.CASCADE)
+    isTA = models.BooleanField(default=False)
 
