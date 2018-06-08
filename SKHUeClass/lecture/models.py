613f8419f59695f1204c20205ac75bbe01c89d7f
Kimjisol <judy134679@gmail.com>
2018-06-08 17:37:55 +0900

    공지작성

--- SKHUeClass/lecture/models.py
+++ SKHUeClass/lecture/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 from account.models import Professor, Student
+from django.utils import timezone
 
 # Create your models here.
 class Lecture(models.Model):
     name = models.CharField(max_length=100)
     professor = models.ForeignKey(Professor, on_delete=models.SET_NULL, null=True)
+
+
+
+class LectureNotice(models.Model):
+    lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100,null=False)
+    content = models.TextField(max_length=1000,null=False)
+    content_at= models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+    file = models.FileField(null=True)
+    limited_date=models.DateTimeField(null=True)
+    is_notice=models.BooleanField()
--- SKHUeClass/lecture/views.py
+++ SKHUeClass/lecture/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Lecture
+from .models import Lecture, LectureNotice
+from django.utils import timezone
 
 
 def my_lecture_list(request):
@@ -19,3 +20,23 @@ def lecture_list(request):
         if request.method == "GET":
             lectures = Lecture.objects.all()
             return render(request, 'lectureApply.html', {'lectures': lectures})
+
+def noticeWrite(request):
+    if request.method =="GET":
+        return render(request,'noticeWrite.html')
+    if request.method=='POST':
+        lec = Lecture.objects.get(id=1)
+        lecNotice = LectureNotice()
+
+        lecNotice.lecture = lec
+        lecNotice.title= request.POST['title']
+        lecNotice.content = request.POST['content']
+        lecNotice.file = request.POST['file']
+        lecNotice.is_notice = request.POST.get('is_notice',False)
+        lecNotice.limited_date = request.POST['limited_date']
+        #lecNotice.content_at = django.utils.timezone.now()
+        lecNotice.updated_at = timezone.now()
+
+
+        lecNotice.save()
+        return render(request,'myLecture.html')
