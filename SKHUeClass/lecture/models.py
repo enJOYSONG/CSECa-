6f3b3c681654d40ed4c5222b9928f026b74c68e0
Kimjisol <judy134679@gmail.com>
2018-06-11 15:27:09 +0900

    Merge branch 'master' of https://github.com/enJOYSONG/SkhuEClass
    # Conflicts:
    #	SKHUeClass/lecture/models.py
    #	SKHUeClass/lecture/urls.py
    #	SKHUeClass/lecture/views.py

--- SKHUeClass/lecture/models.py
+++ SKHUeClass/lecture/models.py
@@ -6,3 +6,13 @@ class Lecture(models.Model):
     name = models.CharField(max_length=100)
     professor = models.ForeignKey(Professor, on_delete=models.SET_NULL, null=True)
     students = models.ManyToManyField(Student)
+
+class LectureNotice(models.Model):
+    lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100,null=False)
+    content = models.TextField(max_length=1000,null=False)
+    content_at= models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+    file = models.FileField(null=True)
+    limited_date=models.DateTimeField(null=True)
+    is_notice=models.BooleanField()
--- SKHUeClass/lecture/views.py
+++ SKHUeClass/lecture/views.py
@@ -60,3 +60,23 @@ def lecture_list(request):
         if request.method == "GET":
             lectures = Lecture.objects.select_related('professor__base_user').all()
             return render(request, 'lectureApply.html', {'lectures': lectures})
+
+def noticeWrite(request):
+    if request.method =="GET":
+        return render(request,'noticeWrite.html')
+    if request.method=='POST':
+        lec = Lecture.objects.get(id=1)
+        lecNotice = LectureNotice()
+
+        lecNotice.lecture = lec
+        lecNotice.title= request.POST['title']
+        lecNotice.content = request.POST['content']
+        lecNotice.file = request.POST['file']
+        lecNotice.is_notice = request.POST.get('is_notice',False)
+        lecNotice.limited_date = request.POST['limited_date']
+        #lecNotice.content_at = django.utils.timezone.now()
+        lecNotice.updated_at = timezone.now()
+
+
+        lecNotice.save()
+        return render(request,'myLecture.html')
