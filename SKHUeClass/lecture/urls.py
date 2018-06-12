6875f8c7df9f72f1959ffb63c222479dd67255d9
KJH\KilJH <roadjh0201@gmail.com>
2018-06-12 18:06:38 +0900

    공지확인 view 및 QnA

--- SKHUeClass/lecture/urls.py
+++ SKHUeClass/lecture/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     url(r'^lecture_detail/(?P<lecture_id>[0-9]+)$', lecture_detail, name='lecture_detail'),
     url(r'^lecture_list$', lecture_list, name='lecture_list'),
     url(r'^noticeWrite/(?P<lecture_id>[0-9]+)$', noticeWrite, name='noticeWrite'),
+    url(r'^noticeView/(?P<notice_id>[0-9]+)$', noticeView, name='noticeView'),
+    url(r'^questionWrite/(?P<lecture_id>[0-9]+)$', questionWrite, name='questionWrite'),
+    url(r'^questionView/(?P<question_id>[0-9]+)$', questionView, name='questionView'),
+    url(r'^commentWrite/(?P<question_id>[0-9]+)$', commentWrite, name='commentWrite'),
+
 ]
